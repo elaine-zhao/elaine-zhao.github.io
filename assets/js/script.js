b200b43c1b087138b0adda066b5b42df312ea6fd
elaine-zhao <elainezha@berkeley.edu>
2015-04-27 09:39:59 -0700

    saved js to one js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,30 @@
-
 $(document).ready(function() {
-                $("#chat_reply").click(function() {
-                    if (!$("#chat_messages-container").hasClass("selected")) {
-                        $("#chat_messages-container").addClass("selected");
-                        $("#chat_messages-container").animate({top: "-=127px"});
-                        $("#chat_reply-container").animate({top: "-=275px"});
-                        $(".chat_row#first").fadeTo("fast", 0);
-                    }
-                });
-            });
 
-}
+    $("#login-button").hover(function() {
+        var password = $("#password").val();
+        if (password == "yaywdd") {
+            $(this).click(function() {return true;})
+        } else {
+            $(this).click(function() {return false;})
+        }
+    });
+
+    $("#start-button").click(function() {
+        $(".before-start").fadeOut();
+    });
+                
+    $("#end-button").click(function() {
+        $(".before-start").fadeIn("fast");
+    });
+
+    $("#chat_reply").click(function() {
+        if (!$("#chat_messages-container").hasClass("selected")) {
+            $("#chat_messages-container").addClass("selected");
+            $("#chat_messages-container").animate({top: "-=127px"});
+            $("#chat_reply-container").animate({top: "-=275px"});
+            $(".chat_row#first").fadeTo("fast", 0);
+        }
+    });
+})
+
+
